4aa193ed4fc2fae5f24031d634f9e15f768bcb93
Juan Miguel <juanmi.marisanchez@gmail.com>
2018-01-29 20:13:18 +0100

    Validadores pasados

--- ExamenEnero/js/Scripts.js
+++ ExamenEnero/js/Scripts.js
@@ -4,20 +4,17 @@ window.onload=function(){
     inf.onclick = function(){
         var section = document.getElementsByTagName("p");
         var texto = document.getElementById("texto");
-        //Restamos 6 ya que son los párrafos que no pertenecen al main
-        var count = section.length - 6;
-        var pa = "En esta página habían "+count+" párrafos y ahora hay "+(count+1)+" párrafos";
+        //Restamos 2 ya que son los párrafos que no pertenecen al main
+        var count = section.length -2;
+        var newParagraph = "En esta página habían "+count+" párrafos y ahora hay "+(count+1)+" párrafos";
 
         for (i=0;i<count;i++){
             if (i == count -1){
                 var h = document.createElement("p");
-                var t = document.createTextNode(pa);
+                var t = document.createTextNode(newParagraph);
                 h.appendChild(t);
                 texto.appendChild(h);
                 
-            }
-            else{
-                section[i].style.display = 'none';
             }
         }
         
