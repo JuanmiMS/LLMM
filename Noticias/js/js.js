9ad261eba371d4d63068fb68f863dd7e8754d519
Juan Miguel <juanmi.marisanchez@gmail.com>
2018-05-23 15:07:14 +0200

    push pc

--- Noticias/js/js.js
+++ Noticias/js/js.js
@@ -7,5 +7,22 @@ $.getJSON('Json/noticias.json', function(data) {
 
 });
 
+$('#desplegar').toggle(
+
+    // Primer click
+    function(e){
+        $('#respuesta-ej5').slideDown();
+        $(this).text('Leer mas');
+        e.preventDefault();
+    }, // Separamos las dos funciones con una coma
+
+    // Segundo click
+    function(e){
+        $('#respuesta-ej5').slideUp();
+        $(this).text('Leer menos');
+        e.preventDefault();
+    }
+
+);
 
 
